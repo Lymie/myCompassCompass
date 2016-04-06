3c2173c07d6c02c0b3b2e3bdc6b70e8553733b32
Lymie <luashley91@gmail.com>
2016-04-06 17:34:35 +0200

    Adding to git
    Adding myCompass files to github

---
+++ MyCompass/app/src/main/java/com/ashleylu/mycompass/MainActivity.java
@@ -0,0 +1,34 @@
+package com.ashleylu.mycompass;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    /** Called when the user clicks the Send button */
+    public void startCompass(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, CompassActivity.class);
+        startActivity(intent);
+
+    }
+
+}
